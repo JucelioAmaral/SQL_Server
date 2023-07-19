c25f1e747596d4ddcdcf876f725d03a0fde64148
jucelio <jucelioinfo@gmail.com>
2023-07-19 19:14:52 -0300

    Criadas mais tabelas e inseridos registros

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,4 @@
---CREATE DATABASE DBDesenvolvedorIO
+-- CREATE DATABASE DBDesenvolvedorIO
 
 use DBDesenvolvedorIO
 go
@@ -60,3 +60,76 @@ DELETE TOP(2) FROM ALUNOS;
 */
 
 ----------------------------------------------------
+/*-- Criando tabela novamente, porem diferente.
+CREATE TABLE Alunos(
+id int PRIMARY KEY IDENTITY,
+nome VARCHAR(80) NOT NULL,
+cidade char(60) NOT NULL,
+estado char(2) NOT NULL,
+cpf CHAR(11) NOT NULL,
+data_nascimento DATE,
+ativo bit DEFAULT 1
+)*/
+
+
+
+/*CREATE TABLE Categorias(
+id int PRIMARY KEY IDENTITY,
+descricao VARCHAR(80) NOT NULL,
+cadastrado_em DATETIME DEFAULT GETDATE()
+)*/
+
+
+/*CREATE TABLE Cursos(
+id int PRIMARY KEY IDENTITY,
+categoria_id INT NOT NULL,
+descricao VARCHAR(80) NOT NULL,
+total_horas INT NOT NULL,
+valor DECIMAL (12,2) NOT NULL DEFAULT 0,
+cadastrado_em DATETIME DEFAULT GETDATE(),
+ativo bit DEFAULT 1,
+CONSTRAINT fx_categoria_id FOREIGN KEY (categoria_id) REFERENCES Categorias(id)
+)*/
+
+
+-- Relacionamento N:N, então criada da tabela com chave composta  (aluno_id, curso_id)
+/*CREATE TABLE Alunos_Cursos(
+aluno_id  INT NOT NULL,
+curso_id  INT NOT NULL,
+cadastrado_em DATETIME DEFAULT GETDATE(),
+CONSTRAINT pk_alunos PRIMARY KEY (aluno_id, curso_id),
+CONSTRAINT fk_aluno_id FOREIGN KEY (aluno_id) REFERENCES Alunos(id),
+CONSTRAINT fk_cursos_id FOREIGN KEY (curso_id) REFERENCES Cursos (id) 
+)*/
+
+
+
+/*INSERT INTO Alunos (nome, cpf, cidade, estado, data_nascimento) VALUES 
+('Rafael','00000000001','Aracaju','SE','2000-01-01'),
+('Eduardo','00000000002','São Paulo','SP','2000-02-02'),
+('Bruno','00000000003','São Paulo','SP','2000-03-03'),
+('Tiago','00000000004','Rio de Janeiro','RJ','2000-04-04'),
+('Heloysa','00000000005','Aracaju','SE','2000-05-05');
+*/
+
+
+/*INSERT INTO Categorias (descricao) VALUES ('Acesso a dados'),('Segurança'),('WEB');*/
+
+
+/*INSERT INTO Cursos (descricao, categoria_id, total_horas, valor) VALUES
+('EF Core',1,17,300),
+('SQL Server',1,5,200),
+('ASP.NET Core Enterprise',3,5,200),
+('Fundamentos de IdentyServer4',2,5,200)*/
+
+
+
+/*INSERT INTO Alunos_Cursos (aluno_id, curso_id) VALUES
+(1,1),
+(1,2),
+(2,3),
+(3,1),
+(4,4),
+(5,1),
+(5,2),
+(5,3);*/
