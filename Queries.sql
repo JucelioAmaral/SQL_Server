6a1a576eeb52f878d09c38b430d761e85233e56c
jucelio <jucelioinfo@gmail.com>
2023-07-19 18:57:45 -0300

    Adicionado arquivo Queries com criação de logs

---
+++ Queries.sql
@@ -0,0 +1,13 @@
+--CREATE DATABASE DBDesenvolvedorIO
+
+use DBDesenvolvedorIO
+go
+
+-- DROP DATABASE DBDesenvolvedorIO
+
+
+/*-- Cria o banco de dados, mais o arquivo do banco de dados e o arquivo de log:
+CREATE DATABASE DBDesenvolvedorIO
+ON (Name = 'Dev_mdf', FILENAME = 'D:\CURSOS E FORMAÇÕES Desenvolvedor.io\SQL para Desenvolvedores\BANCO DE DADOS\teste.mdf')
+LOG ON (Name = 'Dev_log', FILENAME = 'D:\CURSOS E FORMAÇÕES Desenvolvedor.io\SQL para Desenvolvedores\BANCO DE DADOS\teste.ldf')
+*/
